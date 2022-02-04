fd0e697c46150e44caa13219bfa3ee65aedbac67
Isaac <theisaacmj@gmail.com>
2022-02-03 17:04:15 -0800

    About page added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export const Navbar = () => {
   return (
-    <nav className="sticky">
+    <nav className="nav nav--sticky">
       <h1 className="nav__title">Pokedex</h1>
+      <Link to="/" className="nav__item">
+        <p>Home</p>
+      </Link>
+      <Link to="/about" className="nav__item">
+        <p>About</p>
+      </Link>
     </nav>
   );
 };
